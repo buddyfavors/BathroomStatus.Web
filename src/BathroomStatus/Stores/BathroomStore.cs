4bf25901ecfefc26aae1d41d9eb68817b45c4c8d
Buddy Favors Jr <buddyfavors@gmail.com>
2017-02-26 16:21:07 -0500

    Added bathroom store, in memory for now.

---
+++ src/BathroomStatus/Stores/BathroomStore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using BathroomStatus.Models;
+using System.Linq;
+
+namespace BathroomStatus.Stores
+{
+    public class BathroomStore : IBathroomStore
+    {
+        private static readonly List<Bathroom> m_DB = new List<Bathroom>()
+        {
+            new Bathroom
+            {
+                Name = "Back Office"
+            }
+        };
+
+        IEnumerable<Bathroom> IBathroomStore.GetAll()
+        {
+            return m_DB;
+        }
+
+        Bathroom IBathroomStore.GetById(Guid id)
+        {
+            return m_DB.FirstOrDefault(x => x.Id == id);
+        }
+
+        void IBathroomStore.Update(Bathroom bathroom)
+        {
+            var index = m_DB.IndexOf(bathroom);
+            m_DB[index] = bathroom;
+        }
+    }
+}
---
+++ src/BathroomStatus/Stores/IBathroomStore.cs
@@ -0,0 +1,13 @@
+﻿using BathroomStatus.Models;
+using System;
+using System.Collections.Generic;
+
+namespace BathroomStatus.Stores
+{
+    public interface IBathroomStore
+    {
+        IEnumerable<Bathroom> GetAll();
+        Bathroom GetById(Guid id);
+        void Update(Bathroom bathroom);
+    }
+}
