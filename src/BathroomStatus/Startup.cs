0b300a37564081126318e99cfdd36f1e9e0600c5
Buddy Favors Jr <buddyfavors@gmail.com>
2017-02-26 16:21:37 -0500

    Register bathroom store.

--- src/BathroomStatus/Startup.cs
+++ src/BathroomStatus/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using BathroomStatus.Stores;
 
 namespace BathroomStatus
 {
@@ -29,6 +30,8 @@ namespace BathroomStatus
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddSingleton<IBathroomStore, BathroomStore>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
