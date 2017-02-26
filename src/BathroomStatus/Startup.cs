79b0165b6831780ad4584949e013720ef79253e7
Buddy Favors Jr <buddyfavors@gmail.com>
2017-02-26 17:14:40 -0500

    Added some default setup.

--- src/BathroomStatus/Startup.cs
+++ src/BathroomStatus/Startup.cs
@@ -39,8 +39,24 @@ namespace BathroomStatus
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
 
-            app.UseMvc();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
