e25f4290bbccfb030fad1ea76a29c242323cbab3
Buddy Favors Jr <buddyfavors@gmail.com>
2017-02-26 16:21:57 -0500

    Added bathroom api.

---
+++ src/BathroomStatus/Controllers/BathroomsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using BathroomStatus.Stores;
+using BathroomStatus.Models;
+
+namespace BathroomStatus.Controllers
+{
+    [Route("api/[controller]")]
+    public class BathroomsController : Controller
+    {
+        private readonly IBathroomStore m_BathroomStore;
+
+        public BathroomsController(IServiceProvider serviceProvider)
+        {
+            m_BathroomStore = serviceProvider.GetService<IBathroomStore>();
+        }
+
+        [HttpGet]
+        public IEnumerable<Bathroom> Get()
+        {
+            return m_BathroomStore.GetAll();
+        }
+        
+        [HttpPut("{id}")]
+        public void Put(Guid id, [FromBody]Bathroom updatedBathroom)
+        {
+            var bathroom = m_BathroomStore.GetById(id);
+            bathroom.IsOpened = updatedBathroom.IsOpened;
+            m_BathroomStore.Update(bathroom);
+        }
+    }
+}
