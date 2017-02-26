2d1a121fab15f256fc87dd58df6d2cc7b19e186a
Buddy Favors Jr <buddyfavors@gmail.com>
2017-02-26 16:20:33 -0500

    Added bathroom model.

---
+++ src/BathroomStatus/Models/Bathroom.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BathroomStatus.Models
+{
+    public class Bathroom
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public bool IsOpened { get; set; }
+    }
+}
