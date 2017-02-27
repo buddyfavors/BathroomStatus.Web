d5ca1d50cf68f2314561f1581a48624234f2fc1b
Buddy Favors Jr <buddyfavors@gmail.com>
2017-02-26 21:02:38 -0500

    Added bathroom component to render the list of bathrooms.

---
+++ src/BathroomStatus/wwwroot/js/pages/home/bathrooms.jsx
@@ -0,0 +1,37 @@
+﻿var Bathrooms = React.createClass({
+    getInitialState: function () {
+        return { data: [] };
+    },
+    fetchDataFromServer: function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', this.props.url, true);
+        xhr.onload = function () {
+            var data = JSON.parse(xhr.responseText);
+            this.setState({ data: data });
+        }.bind(this);
+        xhr.send();
+    },
+    componentDidMount: function () {
+        this.fetchDataFromServer();
+        window.setInterval(this.fetchDataFromServer, this.props.pollInterval);
+    },
+    render: function () {
+        var bathrooms = this.state.data.map(function (bathroom) {
+            return (
+            <div key={bathroom.id} className="bathroom">
+                {bathroom.name}: <span className={bathroom.isOpened ? 'opened' : 'closed'}>{bathroom.isOpened ? 'Opened' : 'Closed'}</span>
+            </div>
+          );
+        });
+        return (
+          <div>
+            {bathrooms}
+          </div>
+        );
+    }
+});
+
+ReactDOM.render(
+  <Bathrooms url="/api/bathrooms" pollInterval="2000" />,
+  document.getElementById('content')
+);
