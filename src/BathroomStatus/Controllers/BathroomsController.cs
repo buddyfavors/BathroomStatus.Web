3b60f1bcb4f13ff121c6a4294d830b52bebbe687
Buddy Favors Jr <buddyfavors@gmail.com>
2017-02-28 21:26:55 -0500

    When a bathroom is updated, notify connected clients.

--- src/BathroomStatus/Controllers/BathroomsController.cs
+++ src/BathroomStatus/Controllers/BathroomsController.cs
@@ -4,17 +4,25 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using BathroomStatus.Stores;
 using BathroomStatus.Models;
+using Microsoft.AspNetCore.SignalR.Infrastructure;
+using Microsoft.AspNetCore.SignalR;
+using BathroomStatus.Hubs;
 
 namespace BathroomStatus.Controllers
 {
     [Route("api/[controller]")]
     public class BathroomsController : Controller
     {
+        private readonly IConnectionManager m_ConnectionManager;
         private readonly IBathroomStore m_BathroomStore;
+        private readonly IHubContext m_Hub;
 
         public BathroomsController(IServiceProvider serviceProvider)
         {
+            m_ConnectionManager = serviceProvider.GetService<IConnectionManager>();
             m_BathroomStore = serviceProvider.GetService<IBathroomStore>();
+
+            m_Hub = m_ConnectionManager.GetHubContext<BathroomsHub>();
         }
 
         [HttpGet]
@@ -29,6 +37,8 @@ namespace BathroomStatus.Controllers
             var bathroom = m_BathroomStore.GetById(id);
             bathroom.IsOpened = updatedBathroom.IsOpened;
             m_BathroomStore.Update(bathroom);
+
+            m_Hub.Clients.All.Update(bathroom);
         }
     }
 }
