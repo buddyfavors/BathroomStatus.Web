307fd9b80a51ffc8c60cb66bcd770efea046cb38
Buddy Favors Jr <buddyfavors@gmail.com>
2017-02-28 21:26:21 -0500

    Added bathrooms hub.

---
+++ src/BathroomStatus/Hubs/BathroomsHub.cs
@@ -0,0 +1,13 @@
+﻿using BathroomStatus.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BathroomStatus.Hubs
+{
+    public class BathroomsHub : Hub
+    {
+        public void Update(Bathroom bathroom)
+        {
+            Clients.All.Update(bathroom);
+        }
+    }
+}
